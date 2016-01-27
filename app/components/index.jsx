aefe958f743765b31d92d7e8c4e7f0fa49790dfb
Marco Ravazzini <marcoravazzini@marcos-MacBook-Pro.local>
2016-01-28 00:49:34 +0100

    create scaffold

---
+++ app/components/index.jsx
@@ -0,0 +1,9 @@
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    Example = require('./header.jsx');
+
+
+const header = (<Example/>);
+
+
+ReactDOM.render(header, document.getElementById('header'));
